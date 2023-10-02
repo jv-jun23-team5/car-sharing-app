cce3d368dacc1eb5893768eb2e0a8dadc3cdda99
Oleksandr Savenets <134528120+OleksandrSaven@users.noreply.github.com>
2023-10-02 18:36:24 +0300

    renamed file, fixed checkstyle mistakes

--- src/main/java/com/project/carsharingapp/model/User.java
+++ src/main/java/com/project/carsharingapp/model/User.java
@@ -2,23 +2,23 @@ package com.project.carsharingapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
-@Data
+@Setter
+@Getter
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id =?")
 @Where(clause = "is_deleted=false")
 @Table(name = "users")
@@ -40,8 +40,6 @@ public class User {
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
     private Set<Role> roles = new HashSet<>();
     @Column(name = "is_deleted",nullable = false)
     private boolean isDeleted = false;
