0a2bb3346902599133d1acba1a7eb39fe40738a8
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-02 21:21:43 +0300

    added a liquibase file for creating payments table and fixed some checkstyle issues

--- src/main/java/com/project/carsharingapp/model/Payment.java
+++ src/main/java/com/project/carsharingapp/model/Payment.java
@@ -1,7 +1,16 @@
 package com.project.carsharingapp.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
