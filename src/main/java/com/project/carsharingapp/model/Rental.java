5a6569b6eaec738233f2f6e88d83e54cdbf1e17d
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-02 21:06:13 +0300

    adjusted rental entity and added an appropriate liquibase file

--- src/main/java/com/project/carsharingapp/model/Rental.java
+++ src/main/java/com/project/carsharingapp/model/Rental.java
@@ -1,34 +1,35 @@
 package com.project.carsharingapp.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+@Entity
 @Getter
 @Setter
 @SQLDelete(sql = "UPDATE rentals SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted=false")
-@Entity
 @Table(name = "rentals")
 public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "rental_date", nullable = false)
     private LocalDateTime rentalDate;
+    @Column(name = "return_date", nullable = false)
     private LocalDateTime returnDate;
+    @Column(name = "actual_return_date")
     private LocalDateTime actualReturnDate;
-    //@OneToOne(fetch = FetchType.LAZY)
-    //private Car car;
-    // @ManyToOne(fetch = FetchType.LAZY)
-    // private User user;
-    @Column(nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(name = "is_deleted",nullable = false)
     private boolean isDeleted;
 }
