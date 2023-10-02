d242308092b323fa14c51af3e7021a5ea4d9e427
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-02 20:51:31 +0300

    added Payment entity

---
+++ src/main/java/com/project/carsharingapp/model/Payment.java
@@ -0,0 +1,42 @@
+package com.project.carsharingapp.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rental_id", nullable = false)
+    private Rental rentalId;
+    @Column(name = "session_url", nullable = false)
+    private String sessionUrl;
+    @Column(name = "session_id", nullable = false)
+    private String sessionId;
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    public enum Status {
+        PENDING,
+        PAID
+    }
+
+    public enum Type {
+        PAYMENT,
+        FINE
+    }
+}
