bcf0a74d7c4c812129c1050cce985774c2a104f4
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-10-02 13:46:29 +0300

    created main branch

---
+++ src/test/java/com/project/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
