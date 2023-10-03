3275e6d9f201fa63d33d2377c6af62daf788aff3
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-10-03 12:05:35 +0300

    added notification service and telegram bot

--- src/main/java/com/project/carsharingapp/repository/UserRepository.java
+++ src/main/java/com/project/carsharingapp/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.project.carsharingapp.repository;
 
 import com.project.carsharingapp.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
