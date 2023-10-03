256d627c6484c2889505afb3f8c2a12394c03852
Dmytro Varukha <132067460+DmytroV95@users.noreply.github.com>
2023-10-03 09:34:17 +0300

    [USER CONTROLLER] Created user controller and user DTO
    * created car controller
    
    * fixed checkstyle

---
+++ src/main/java/com/project/carsharingapp/dto/car/UpdateCarRequestDto.java
@@ -0,0 +1,31 @@
+package com.project.carsharingapp.dto.car;
+
+import com.project.carsharingapp.model.Car;
+import java.math.BigDecimal;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class UpdateCarRequestDto {
+    @NotNull
+    @Length(min = 1, max = 255)
+    private String model;
+    @NotNull
+    @Length(min = 1, max = 255)
+    private String brand;
+    @NotBlank
+    @NotNull
+    @Min(0)
+    private Integer inventory;
+    @NotBlank
+    @NotNull
+    private Car.CarType carType;
+    @NotBlank
+    @Min(0)
+    private BigDecimal dailyFee;
+}
