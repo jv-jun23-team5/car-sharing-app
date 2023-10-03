5485ac4e50660bbf9989c0e5d955f86ce1cf3043
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-10-03 16:05:13 +0300

    "[Car] added improvements, check test: all endpoints is working!"

--- src/main/java/com/project/carsharingapp/dto/car/UpdateCarRequestDto.java
+++ src/main/java/com/project/carsharingapp/dto/car/UpdateCarRequestDto.java
@@ -2,7 +2,6 @@ package com.project.carsharingapp.dto.car;
 
 import com.project.carsharingapp.model.Car;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
