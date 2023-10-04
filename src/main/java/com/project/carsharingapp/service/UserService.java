fac44438bf2f10643b10d1908ed5a97ece239fb9
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-10-04 13:27:42 +0300

    "[Security] refactored merged conflicts"

--- src/main/java/com/project/carsharingapp/service/UserService.java
+++ src/main/java/com/project/carsharingapp/service/UserService.java
@@ -1,9 +1,9 @@
 package com.project.carsharingapp.service;
 
-import com.project.carsharingapp.dto.user.UserRegistrationRequestDto;
-import com.project.carsharingapp.dto.user.UserResponseDto;
 import com.project.carsharingapp.dto.user.UpdateUserProfileRequestDto;
 import com.project.carsharingapp.dto.user.UpdateUserRoleRequestDto;
+import com.project.carsharingapp.dto.user.UserRegistrationRequestDto;
+import com.project.carsharingapp.dto.user.UserResponseDto;
 import com.project.carsharingapp.exception.RegistrationException;
 import org.springframework.stereotype.Service;
 
