ef782f4065f667cacda2996fd801c8961b550c94
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-04 20:05:40 +0300

    fixed issues in payment controller

--- src/main/java/com/project/carsharingapp/controller/PaymentController.java
+++ src/main/java/com/project/carsharingapp/controller/PaymentController.java
@@ -7,13 +7,9 @@ import com.project.carsharingapp.service.PaymentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
-import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
