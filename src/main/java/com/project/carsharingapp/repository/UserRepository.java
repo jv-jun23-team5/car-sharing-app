96aa78ffe10af59919427ef710b5b0afce599c94
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-04 14:47:44 +0300

    made some small changes

--- src/main/java/com/project/carsharingapp/repository/UserRepository.java
+++ src/main/java/com/project/carsharingapp/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.project.carsharingapp.repository;
 
 import com.project.carsharingapp.model.User;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    //@EntityGraph(attributePaths = "roles")
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 
     @Query("FROM User u LEFT JOIN FETCH u.roles WHERE u.id = :id")
