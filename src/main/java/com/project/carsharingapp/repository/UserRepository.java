177fe87eed3b8044a09ffd8583f4a7d78f57ee23
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-04 14:50:14 +0300

    changed user repository method

--- src/main/java/com/project/carsharingapp/repository/UserRepository.java
+++ src/main/java/com/project/carsharingapp/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.project.carsharingapp.repository;
 
 import com.project.carsharingapp.model.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
