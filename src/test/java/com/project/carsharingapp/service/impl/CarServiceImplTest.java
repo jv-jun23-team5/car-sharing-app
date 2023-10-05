1674d2fb95021aff9a0fc203932b065467a50db5
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-10-05 18:19:21 +0300

    fixed checkstyle problems

--- src/test/java/com/project/carsharingapp/service/impl/CarServiceImplTest.java
+++ src/test/java/com/project/carsharingapp/service/impl/CarServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.project.carsharingapp.service;
+package com.project.carsharingapp.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
