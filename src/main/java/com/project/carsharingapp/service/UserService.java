8ced2125e16f7c2298ae7ba07acf5c3e77dda3e4
OlegStashkiv <40997183+OlegStashkiv@users.noreply.github.com>
2023-10-05 12:34:59 +0300

    [USER] Added UserRegisterResponseDto (#29)
    * added notification to RentalServiceImpl.java
    
    * added UserRegisterResponseDto for register answer
    
    * fixed checkstyle
    
    * renamed RentalRepository method
    
    * tried to resolve conflict
    
    * tried to resolve conflict: modified RentalServiceImpl.java

--- src/main/java/com/project/carsharingapp/service/UserService.java
+++ src/main/java/com/project/carsharingapp/service/UserService.java
@@ -2,6 +2,7 @@ package com.project.carsharingapp.service;
 
 import com.project.carsharingapp.dto.user.UpdateUserProfileRequestDto;
 import com.project.carsharingapp.dto.user.UpdateUserRoleRequestDto;
+import com.project.carsharingapp.dto.user.UserRegisterResponseDto;
 import com.project.carsharingapp.dto.user.UserRegistrationRequestDto;
 import com.project.carsharingapp.dto.user.UserResponseDto;
 import com.project.carsharingapp.exception.RegistrationException;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    UserResponseDto register(UserRegistrationRequestDto registrationRequestDto)
+    UserRegisterResponseDto register(UserRegistrationRequestDto registrationRequestDto)
             throws RegistrationException;
 
     UserResponseDto updateUserProfile(Long id, UpdateUserProfileRequestDto requestDto);
