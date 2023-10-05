12cf810a3a3349c614a2010fd609dbb3c8f660fc
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-05 13:44:13 +0300

    added cron param

--- src/main/java/com/project/carsharingapp/service/impl/SchedulerServiceImp.java
+++ src/main/java/com/project/carsharingapp/service/impl/SchedulerServiceImp.java
@@ -1,17 +1,19 @@
 package com.project.carsharingapp.service.impl;
 
 import com.project.carsharingapp.service.SchedulerService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SchedulerServiceImp implements SchedulerService {
     @Override
     public void checkOverdueRentals() {
-
+        System.out.println("SCHEDULED TELEGRAM");
     }
 
     @Override
+    @Scheduled(cron = "*/1 * * * *" )
     public void checkExpiredPaymentSessions() {
-
+        System.out.println("SCHEDULED PAYMENT");
     }
 }
